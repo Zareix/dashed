73743e099328fb2fd399d0c66194f2a8bbf3096f
Raphaël Catarino <raphcatarino@gmail.com>
2022-04-06 23:16:32 +0200

    fix docker

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,9 @@ RUN yarn global add servor
 
 WORKDIR /app
 COPY . .
+
 RUN chmod +x docker-start.sh
 
 EXPOSE 8080
 
-CMD ["sh", "/app/docker-start.sh"]
+CMD ["/bin/sh", "/app/docker-start.sh"]
--- docker-start.sh
+++ docker-start.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-if [[ ! -e /app/node_modules ]]; then
+if [ ! -e /app/node_modules ]; then
     yarn install
 fi
-if [[ ! -e /app/dist ]]; then
+if [ ! -e /app/dist ]; then
     mkdir -p /app/dist
 fi
-if [[ ! -e /app/public ]]; then
+if [ ! -e /app/public ]; then
     mkdir -p /app/public
 fi
-if [[ ! -e /app/public/assets ]]; then
+if [ ! -e /app/public/assets ]; then
     mkdir -p /app/public/assets
 fi
-if [[ ! -e /app/public/data.json ]]; then
+if [ ! -e /app/public/data.json ]; then
     cp /app/src/defaults/data-default.json /app/public/data.json
 fi
 servor dist --reload &
