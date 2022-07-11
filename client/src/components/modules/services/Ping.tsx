b878c161c5a9055ca024faa16587d006f882b71d
Raphaël Catarino <raphcatarino@gmail.com>
2022-07-11 14:49:29 +0200

    ping service

--- client/src/components/modules/services/Ping.tsx
+++ client/src/components/modules/services/Ping.tsx
@@ -9,6 +9,7 @@ type Props = {
 };
 
 const Ping = ({ app }: Props) => {
+  const allowedStatusCode = app.allowedStatusCode ?? [200];
   const { isLoading, data: statusCode } = useQuery(
     ["ping", app.name],
     () => ping(app.endpoint ?? app.url, app.headers),
@@ -19,8 +20,7 @@ const Ping = ({ app }: Props) => {
 
   if (isLoading && !statusCode) return <></>;
 
-  if (statusCode === 200 || app.allowedStatusCode?.includes(statusCode ?? 0))
-    return <Enabled />;
+  if (allowedStatusCode?.includes(statusCode ?? 0)) return <Enabled />;
   return <Disabled />;
 };
 
