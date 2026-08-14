0cde80a71cf1b2149e31c62d821b9ffe7723da33
Raphaël C. <raphcatarino@gmail.com>
2026-08-14 18:33:44 +0200

    fix: size claude usage

--- src/components/service/widget/widgets/claude-usage.tsx
+++ src/components/service/widget/widgets/claude-usage.tsx
@@ -5,7 +5,7 @@ type Props = {
 }
 export const ClaudeUsageWidget = ({ config }: Props) => {
   return (
-    <div className="fullscreen h-60 w-sm overflow-hidden rounded-md border">
+    <div className="fullscreen h-78 w-sm overflow-hidden rounded-md border">
       <iframe
         src={config.url}
         title="Claude Usage"
